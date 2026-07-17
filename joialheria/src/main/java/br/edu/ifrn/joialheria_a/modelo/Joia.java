d3fa2403ea485b4263b54db68024598114a3ec16
ribeirolindalva48-rgb <ribeirolindalva48@gmail.com>
2026-07-16 21:50:20 -0300

    Update Joia.java

--- joialheria/src/main/java/br/edu/ifrn/joialheria_a/modelo/Joia.java
+++ joialheria/src/main/java/br/edu/ifrn/joialheria_a/modelo/Joia.java
@@ -1,53 +1,78 @@
 package br.edu.ifrn.joialheria_a.modelo;
 
-import java.util.List;
-
+/**
+ * Entidade que representa uma Joia.
+ * Mapeia diretamente os dados da tabela do MySQL.
+ */
 public class Joia {
 
+    private Long id; // Chave primária do MySQL
     private String nome;
-    private List<String> fotos;
     private String teorOuro;
     private String tipoGema;
     private double peso;
 
+    // Construtor vazio
+    public Joia() {
+    }
 
-    public Joia(String nome, List<String> fotos,
-                String teorOuro,
-                String tipoGema,
-                double peso) {
-
+    // Construtor completo
+    public Joia(String nome, String teorOuro, String tipoGema, double peso) {
         this.nome = nome;
-        this.fotos = fotos;
         this.teorOuro = teorOuro;
         this.tipoGema = tipoGema;
         this.peso = peso;
     }
 
-
-    public boolean possuiFotos() {
-
-        return fotos != null && !fotos.isEmpty();
-
+    // Getters e Setters
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return nome;
     }
 
-
-    public double getPeso() {
-        return peso;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
-
 
     public String getTeorOuro() {
         return teorOuro;
     }
 
+    public void setTeorOuro(String teorOuro) {
+        this.teorOuro = teorOuro;
+    }
 
     public String getTipoGema() {
         return tipoGema;
     }
 
+    public void setTipoGema(String tipoGema) {
+        this.tipoGema = tipoGema;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Joia{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", teorOuro='" + teorOuro + '\'' +
+                ", tipoGema='" + tipoGema + '\'' +
+                ", peso=" + peso +
+                '}';
+    }
 }
