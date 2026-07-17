0168523971543b99d44f7ba2e0aa4474d9434bd2
ribeirolindalva48-rgb <ribeirolindalva48@gmail.com>
2026-07-16 22:19:33 -0300

    Update GerenciadorDeConexao.java

--- joialheria/src/main/java/br/edu/ifrn/joialheria_a/repositório/GerenciadorDeConexao.java
+++ joialheria/src/main/java/br/edu/ifrn/joialheria_a/repositório/GerenciadorDeConexao.java
@@ -1,30 +1,16 @@
-
 package br.edu.ifrn.joialheria_a.repositorio;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * Classe responsável pela conexão com o banco de dados MySQL.
- */
 public class GerenciadorDeConexao {
 
-    // URL do banco de dados da Joalheria
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/joialheria_db?useTimezone=true&serverTimezone=UTC";
-
-    // Altere para o usuário do seu MySQL
+    private static final String URL = "jdbc:mysql://localhost:3306/joialheria_db";
     private static final String USER = "root";
+    private static final String PASSWORD = "@Emmycami1607";
 
-    // Altere para a senha do seu MySQL
-    private static final String PASSWORD = "root";
-
-    /**
-     * Retorna uma conexão ativa com o banco de dados.
-     */
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
-
 }
